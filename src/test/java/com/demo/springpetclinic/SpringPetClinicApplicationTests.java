47d7e954abddaf820a9a4074ee1d5efe9de15d12
Rohan Basu <basurohan@gmail.com>
2020-02-14 23:43:06 -0500

    Initial Project Add

---
+++ src/test/java/com/demo/springpetclinic/SpringPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.springpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
