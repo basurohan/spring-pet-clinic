a48ca650512fbacb9da425ee694e920062615886
Rohan Basu <basurohan@gmail.com>
2020-02-16 00:59:29 -0500

    Implement interfaces for services. Closes #5.

---
+++ pet-clinic-data/src/main/java/com/demo/springpetclinic/services/PetService.java
@@ -0,0 +1,14 @@
+package com.demo.springpetclinic.services;
+
+import com.demo.springpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+}
---
+++ pet-clinic-data/src/main/java/com/demo/springpetclinic/services/VetService.java
@@ -0,0 +1,15 @@
+package com.demo.springpetclinic.services;
+
+import com.demo.springpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+
+}
