366c31edc234943e1ecd6c84e3a9959671610709
Rohan Basu <basurohan@gmail.com>
2020-02-26 23:40:26 -0500

    Create spring data JPA to vet specialty service, closes #44.

--- pet-clinic-data/src/main/java/com/demo/springpetclinic/services/map/PetTypeMapService.java
+++ pet-clinic-data/src/main/java/com/demo/springpetclinic/services/map/PetTypeMapService.java
@@ -1,31 +1,30 @@
 package com.demo.springpetclinic.services.map;
 
-import com.demo.springpetclinic.model.Specialty;
-import com.demo.springpetclinic.services.SpecialtyService;
+import com.demo.springpetclinic.model.PetType;
+import com.demo.springpetclinic.services.PetTypeService;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
-public class SpecialtyServiceMap extends AbstractMapService<Specialty, Long> implements SpecialtyService {
-
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
     @Override
-    public Specialty findById(Long id) {
+    public PetType findById(Long id) {
         return super.findById(id);
     }
 
     @Override
-    public Set<Specialty> findAll() {
+    public Set<PetType> findAll() {
         return super.findAll();
     }
 
     @Override
-    public Specialty save(Specialty object) {
+    public PetType save(PetType object) {
         return super.save(object);
     }
 
     @Override
-    public void delete(Specialty object) {
+    public void delete(PetType object) {
         super.delete(object);
     }
 
---
+++ pet-clinic-data/src/main/java/com/demo/springpetclinic/services/springdatajpa/SpecialtySpringDataJpaService.java
@@ -0,0 +1,48 @@
+package com.demo.springpetclinic.services.springdatajpa;
+
+import com.demo.springpetclinic.model.Specialty;
+import com.demo.springpetclinic.repositories.SpecialtyRepository;
+import com.demo.springpetclinic.services.SpecialtyService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class SpecialtySpringDataJpaService implements SpecialtyService {
+
+    private final SpecialtyRepository specialtyRepository;
+
+    public SpecialtySpringDataJpaService(SpecialtyRepository specialtyRepository) {
+        this.specialtyRepository = specialtyRepository;
+    }
+
+    @Override
+    public Specialty findById(Long aLong) {
+        return specialtyRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Set<Specialty> findAll() {
+        Set<Specialty> specialties = new HashSet<>();
+        specialtyRepository.findAll().forEach(specialties::add);
+        return specialties;
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return specialtyRepository.save(object);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        specialtyRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        specialtyRepository.deleteById(aLong);
+    }
+}
