b07bb52841b602d18e63843b4e63cdda7baeec80
Rohan Basu <basurohan@gmail.com>
2020-02-27 22:08:46 -0500

    Create Visit Map Service, closes #46

---
+++ pet-clinic-data/src/main/java/com/demo/springpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package com.demo.springpetclinic.services;
+
+import com.demo.springpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/demo/springpetclinic/services/map/VisitMapService.java
@@ -0,0 +1,39 @@
+package com.demo.springpetclinic.services.map;
+
+import com.demo.springpetclinic.model.Visit;
+import com.demo.springpetclinic.services.VisitService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        if (object.getPet() == null || object.getPet().getOwner() == null || object.getPet().getId() == null) {
+            throw new RuntimeException("Invalid Object - Visit");
+        }
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
